ebd84a4ec782857c70dc5a3c8054b71b890bf416
JJLo1 <82445764+JJLo1@users.noreply.github.com>
2021-04-25 13:53:50 +0100

    Added init functionality for IttCalculator

--- calculator.py
+++ calculator.py
@@ -1,1 +1,23 @@
 # your implementation here
+import sys
+from PyQt5 import uic, QtWidgets, QtCore, Qt
+
+
+class IttCalculator(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.ui = uic.loadUi("calculator.ui", self)
+        self.show()
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    calculator = IttCalculator()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
