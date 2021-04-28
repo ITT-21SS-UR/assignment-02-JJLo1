0d3b53af91c9d0a242a95187cd12aa224d8a79dc
JJLo1 <82445764+JJLo1@users.noreply.github.com>
2021-04-28 21:00:25 +0100

    added display and calculating functions, added display updates, removed some overhead

--- calculator.py
+++ calculator.py
@@ -3,10 +3,11 @@ import sys
 from PyQt5 import uic, QtWidgets, QtCore
 
 
+
 def input_logging_decorator(function):
     def wrapper(*args, **kwargs):
         print('New input via {}: {}'.format(function.__name__, args[1]))
-        return function
+        return function(*args, **kwargs)
 
     return wrapper
 
@@ -15,80 +16,85 @@ class IttCalculator(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.ui = uic.loadUi("calculator.ui", self)
-        self.new_input = None
+        self.equationText = ""
+        self.equationLabel = self.ui.EquationLabel
+        self.resultText = ""
+        self.resultLabel = self.ui.ResultLabel
         self.init_buttons()
         self.show()
 
+    def add_to_expression(self, new_input):
+        self.equationText += new_input
+        self.equationLabel.setText(self.equationText)
 
-    def add_input(self, new_input):
-        self.new_input = new_input
+    def execute_command(self, command):
+        if command == "enter":
+            self.resultText = self.calculate_result()
+            self.resultLabel.setText(self.resultText)
+        elif command == "clear":
+            self.equationText = ""
+            self.equationLabel.setText(self.equationText)
+        elif command == "backspace":
+            self.equationText = self.equationText[:-1]
+            self.equationLabel.setText(self.equationText)
+
+    def calculate_result(self):
+        try:
+            result = str(eval(self.equationText))
+            return result
+        except:
+            return "Err"
 
     @input_logging_decorator
-    def keyboard_input(self, new_input):
-        self.add_input(new_input)
+    def keyboard_input_command(self, new_input):
+        self.execute_command(new_input)
 
     @input_logging_decorator
-    def mouse_input(self, button):
-        if button == "1":
-            self.add_input(self, 1)
+    def mouse_input_command(self, button):
+        self.execute_command(button)
+
+    @input_logging_decorator
+    def keyboard_input_number_or_operator(self, new_input):
+        self.add_to_expression(new_input)
+
+    @input_logging_decorator
+    def mouse_input_number_or_operator(self, button):
+        self.add_to_expression(button)
 
     def init_buttons(self):
-        self.ui.NumButton_1.clicked.connect(lambda x: self.mouse_input("1"))
-        self.ui.NumButton_2.clicked.connect(lambda x: self.mouse_input("2"))
-        self.ui.NumButton_3.clicked.connect(lambda x: self.mouse_input("3"))
-        self.ui.NumButton_4.clicked.connect(lambda x: self.mouse_input("4"))
-        self.ui.NumButton_5.clicked.connect(lambda x: self.mouse_input("5"))
-        self.ui.NumButton_6.clicked.connect(lambda x: self.mouse_input("6"))
-        self.ui.NumButton_7.clicked.connect(lambda x: self.mouse_input("7"))
-        self.ui.NumButton_8.clicked.connect(lambda x: self.mouse_input("8"))
-        self.ui.NumButton_9.clicked.connect(lambda x: self.mouse_input("9"))
-        self.ui.NumButton_0.clicked.connect(lambda x: self.mouse_input("0"))
-        self.ui.NumButton_DecPoint.clicked.connect(lambda x: self.mouse_input("."))
-        self.ui.NumButton_Clear.clicked.connect(lambda x: self.mouse_input("clear"))
-        self.ui.NumButton_Delete.clicked.connect(lambda x: self.mouse_input("backspace"))
-        self.ui.NumButton_Enter.clicked.connect(lambda x: self.mouse_input("enter"))
-        self.ui.NumButton_Divide.clicked.connect(lambda x: self.mouse_input("/"))
-        self.ui.NumButton_Multiply.clicked.connect(lambda x: self.mouse_input("*"))
-        self.ui.NumButton_Add.clicked.connect(lambda x: self.mouse_input("+"))
-        self.ui.NumButton_Subtract.clicked.connect(lambda x: self.mouse_input("-"))
+        self.ui.NumButton_1.clicked.connect(lambda x: self.mouse_input_number_or_operator("1"))
+        self.ui.NumButton_2.clicked.connect(lambda x: self.mouse_input_number_or_operator("2"))
+        self.ui.NumButton_3.clicked.connect(lambda x: self.mouse_input_number_or_operator("3"))
+        self.ui.NumButton_4.clicked.connect(lambda x: self.mouse_input_number_or_operator("4"))
+        self.ui.NumButton_5.clicked.connect(lambda x: self.mouse_input_number_or_operator("5"))
+        self.ui.NumButton_6.clicked.connect(lambda x: self.mouse_input_number_or_operator("6"))
+        self.ui.NumButton_7.clicked.connect(lambda x: self.mouse_input_number_or_operator("7"))
+        self.ui.NumButton_8.clicked.connect(lambda x: self.mouse_input_number_or_operator("8"))
+        self.ui.NumButton_9.clicked.connect(lambda x: self.mouse_input_number_or_operator("9"))
+        self.ui.NumButton_0.clicked.connect(lambda x: self.mouse_input_number_or_operator("0"))
+        self.ui.NumButton_DecPoint.clicked.connect(lambda x: self.mouse_input_number_or_operator("."))
+        self.ui.NumButton_Divide.clicked.connect(lambda x: self.mouse_input_number_or_operator("/"))
+        self.ui.NumButton_Multiply.clicked.connect(lambda x: self.mouse_input_number_or_operator("*"))
+        self.ui.NumButton_Add.clicked.connect(lambda x: self.mouse_input_number_or_operator("+"))
+        self.ui.NumButton_Subtract.clicked.connect(lambda x: self.mouse_input_number_or_operator("-"))
+        self.ui.NumButton_Clear.clicked.connect(lambda x: self.mouse_input_command("clear"))
+        self.ui.NumButton_Delete.clicked.connect(lambda x: self.mouse_input_command("backspace"))
+        self.ui.NumButton_Enter.clicked.connect(lambda x: self.mouse_input_command("enter"))
 
     def keyPressEvent(self, event):
-        if event.text() == "1":
-            self.keyboard_input("1")
-        elif event.text() == "2":
-            self.keyboard_input("2")
-        elif event.text() == "3":
-            self.keyboard_input("3")
-        elif event.text() == "4":
-            self.keyboard_input("4")
-        elif event.text() == "5":
-            self.keyboard_input("5")
-        elif event.text() == "6":
-            self.keyboard_input("6")
-        elif event.text() == "7":
-            self.keyboard_input("7")
-        elif event.text() == "8":
-            self.keyboard_input("8")
-        elif event.text() == "9":
-            self.keyboard_input("9")
-        elif event.text() == "0":
-            self.keyboard_input("0")
-        elif event.text() == "/":
-            self.keyboard_input("/")
-        elif event.text() == "*":
-            self.keyboard_input("*")
-        elif event.text() == "+":
-            self.keyboard_input("+")
-        elif event.text() == "-":
-            self.keyboard_input("-")
+        allowed_numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+        allowed_operators = ["/", "*", "+", "-"]
+
+        if event.text() in allowed_numbers:
+            self.keyboard_input_number_or_operator(event.text())
+        elif event.text() in allowed_operators:
+            self.keyboard_input_number_or_operator(event.text())
         elif event.key() == QtCore.Qt.Key_Enter or event.key() == QtCore.Qt.Key_Return:
-            self.keyboard_input("enter")
+            self.keyboard_input_command("enter")
         elif event.key() == QtCore.Qt.Key_Backspace:
-            self.keyboard_input("backspace")
+            self.keyboard_input_command("backspace")
         elif event.text() == "." or event.text() == ",":
-            self.keyboard_input(".")
-
-
+            self.keyboard_input_number_or_operator(".")
 
 
 def main():
